54fdb2bc40a17eb0043dd1635da9b9c664ff92bb
priyanshu082 <priyanshusingh216@gmail.com>
2023-07-25 23:51:34 +0530

    +1

--- ARRAYS/HARD/MergingTwoSortedArrays.cpp
+++ ARRAYS/HARD/MergingTwoSortedArrays.cpp
@@ -22,6 +22,42 @@ void mergeTwoSortedArraysWithoutExtraSpace(vector<long long> &a, vector<long lon
 	sort(b.begin(),b.end());
 }
 	
+//optimal-2 gap method.....shell sort
+void SwapGreater(vector<long long>& arr1, vector<long long>& arr2, int ind1, int ind2) {
+  if (arr1[ind1] > arr2[ind2]) {
+    swap(arr1[ind1], arr2[ind2]);
+  }
+}
+
+void mergeTwoSortedArraysWithoutExtraSpace(vector<long long> &a,
+                                           vector<long long> &b) {
+  int n = a.size();
+  int m = b.size();
+  int len = m + n;
+  int gap = (len / 2) + (len % 2);
+  while (gap > 0) {
+    int left = 0;
+    int right = left + gap;
+    while (right < len) {
+      // arr1 and arr2
+      if (left < n && right >= n) {
+		  SwapGreater( a,  b, left, right-n);
+      }
+	  //arr2 and arr2
+     else if (left >=n) {
+		  SwapGreater (a,  b, left-n, right-n);
+      }
+	  //arr1 and arr1
+      else {
+		  SwapGreater (a, b, left, right);
+      }
+	  right++,left++;
+    }
+	if(gap==1) break;
+	gap=(gap/2)+(gap%2);
+  }
+}
+
 
 int main(){
 
