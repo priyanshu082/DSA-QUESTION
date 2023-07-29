b513d4b3ea39bf46aed9f7f03039399fae2f93a2
priyanshu082 <priyanshusingh216@gmail.com>
2023-07-29 22:37:21 +0530

    +1

---
+++ BINARY SEARCH/LowerBound.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+//using binary search 
+int lowerBound(vector<int> arr, int n, int x) {
+  int low = 0;
+  int high = n - 1;
+  int mid = 0;
+  while (low < high) {
+    mid = (low + high) / 2;
+	 if (arr[mid] == x) return mid;
+    else if (x > arr[mid]) {
+      low = mid + 1;
+    }
+    else high = mid - 1;
+  }
+  if(high==low){
+	  if(x<arr[high]) return high;
+	  else{
+		  return high+1;
+	  }
+  }
+  else if(low>high){
+	  return low;
+  }
+
+}
+
+
+int main(){
+
+    return 0;
+}
