25a5ce9db8d0f0964e138502a272f51393dcea7a
priyanshu082 <priyanshusingh216@gmail.com>
2023-09-08 09:01:13 +0530

    +1

---
+++ STRINGS/ReverseTheString.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+void reverseString(vector<char>& s) {
+        int l=0;
+        int h=s.size()-1;
+        while(l<h){
+            swap(s[l++],s[h--]);
+        }
+    }
+
+int main(){
+
+    return 0;
+}
