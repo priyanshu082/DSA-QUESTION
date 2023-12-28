89676996ef4f6d3a1785c88c0f5d4e200556b654
priyanshu082 <priyanshusingh216@gmail.com>
2023-12-28 11:40:51 +0530

    +1

---
+++ LINKEDLIST/EASY/ReverseDoublyLL.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+using namespace std;
+
+class Node{
+public:
+int data;
+Node * next;
+Node* prev;
+Node(int val):data(val),next(nullptr),prev(nullptr){}
+};
+
+Node* reverseDLL(Node* head)
+{   
+    // Write your code here
+    Node * current =head;
+    Node * temp=nullptr;
+
+
+   while(current){
+       temp=current->prev;
+       current->prev=current->next;
+       current->next=temp;
+       current=current->prev;
+   }
+
+    if(temp) head=temp->prev;
+   return head;
+
+}
+
+
+int main(){
+
+    return 0;
+}
