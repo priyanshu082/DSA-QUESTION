8a36b542f5fe32e1c0e2eedb4c30453aaf3cc589
priyanshu082 <priyanshusingh216@gmail.com>
2024-01-14 11:55:50 +0530

    +1

---
+++ LINKEDLIST/MEDIUM/reverseLL.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//          if(!head  || !head->next) return head;
+
+//          vector<int> data;
+
+//          ListNode* temp=head;
+       
+//          while(temp!=nullptr){
+//              data.push_back(temp->val);
+//              temp=temp->next;
+//          }
+
+//          temp=head;
+//          int cnt=data.size();
+//          for(int i=0;i<cnt;i++){
+//              temp->val=data[cnt-i-1];
+//              temp=temp->next;
+//          }
+
+//          return head;
+//     }
+// };
+
+int main(){
+
+    return 0;
+}
