0b2d5b963f1b4e44f6b0bcadec48631f56dd2fb1
priyanshu082 <priyanshusingh216@gmail.com>
2025-04-05 21:45:15 +0530

    Disjoint set by size

--- GRAPHS/MST_DISJOINTS/disjoint.cpp
+++ GRAPHS/MST_DISJOINTS/disjoint.cpp
@@ -3,9 +3,10 @@ using namespace std;
 
 class DisjointSet{
     public:
-    vector<int> rank,parent;
+    vector<int> rank,parent,size;
     DisjointSet(int n){
         rank.resize(n+1,0);
+        size.resize(n+1);
         parent.resize(n+1,0);
         for(int i =0;i<=n;i++){
             parent[i]=i;
@@ -30,6 +31,19 @@ class DisjointSet{
             parent[ulp_v]=ulp_u;
             rank[ulp_u]++;
         }
+    }
+
+    void unionBySize(int u,int v){
+        int ulp_u=findUPar(u);
+        int ulp_v=findUPar(v);
+        if(ulp_u==ulp_v) return ;
+        if(size[ulp_u]<size[ulp_v]){
+            parent[ulp_u]=ulp_v;
+            size[ulp_v]+=size[ulp_u];
+        } else {
+           parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        } 
     }
    
     
