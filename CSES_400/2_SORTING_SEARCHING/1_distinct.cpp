3c1e77ba632c5143b5a6c177af76d5acb0388de9
priyanshu082 <priyanshusingh216@gmail.com>
2025-07-20 23:01:49 +0530

    +1

---
+++ CSES_400/2_SORTING_SEARCHING/1_distinct.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<set>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    set<int> st;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        st.insert(x);
+    }
+    cout << st.size() << endl;
+}
